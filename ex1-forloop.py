17667fc3b916f718fe398a0c5e365e147353f9c1
Aathishwaran-003 <jaathish004.info@gmail.com>
2025-05-28 11:38:53 +0530

    2nd commit

---
+++ ex1-forloop.py
@@ -0,0 +1,10 @@
+#count to n and print ech number
+n=int(input("Enter the value of n:"))
+def count-to-n(x=n):
+    for i range (i,x+1):
+        print(i,end='')
+        print()
+
+        print("going to count to {}...".format(n))
+        count-to-n(n)
+        print("first modifiction of 2nd commit")
---
+++ ex2-dfunction.py
@@ -0,0 +1,8 @@
+def double(n):
+    return 2*n   #return twice the given number
+    #call the function with value of your choice and print its result
+    print("to call the function to print the number")
+    #x=double(3)
+    #print(x)
+    print("the program ran successfully")
+    print("first modifiction for 2nd commit")
